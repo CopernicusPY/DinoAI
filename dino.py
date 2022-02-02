95faaa9b257000fcbd3404440b9cef49cdfa0323
Capernicus <80885284+CapernicusPY@users.noreply.github.com>
2022-02-02 15:50:53 +0200

    Update dino.py

--- dino.py
+++ dino.py
@@ -1,6 +1,5 @@
-
 #As of now this script captures data from screen that can be customized by the user, and uses image annotation to display the pixel value that the user is interested in to the screen.
-#I may turn this into a class for a more user friendly expirience when the AI is done.
+#Day/Night detection added. This will help later for the cactus-detection system.
 #Image Manipulation-Related Functions
 
 import cv2 as cv
@@ -15,9 +14,15 @@ def threshold(v1, v2, region):
     return (thresh, src)
 def show_pixel_val(img,text_pos, pixel):
     cv2.putText(img,str(img[pixel[0],pixel[1]),(text_pos[0], text_pos[1]), cv2.FONT_HERSHEY_PLAIN, 2, (83,83,83), 2)
+def is_day(img, x, y):
+    if img[x, y] == 225:
+        return True
+    else:
+        return False
 while True:
     img = threshold(127, 255, (51,226,375,202))
     show_pixel_val(img,(141,78),(0,200))
+    is_day(img, 0, 159)
                             
     cv2.imshow("Window Capture:", img) 
 
